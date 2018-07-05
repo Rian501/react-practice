c9c97299b62d27a4804dc1f6031e558c9596c9d3
Emily Lemmon <eclaire.lemmon@gmail.com>
2018-07-05 13:10:02 -0500

    first commit

---
+++ kennel/src/LocationList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList extends Component {
+  render() {
+    return (
+      <article>
+        <h1>Locations</h1>
+        <h3>Student Kennels</h3>
+        <h4>Nashville North Location</h4>
+        <h5>500 Puppy Way</h5>
+        <h3>Faculty Kennels</h3>
+        <h4>Nashville South Location</h4>
+        <h5>500 Old Doggo Way</h5>
+      </article>
+    );
+  }
+}
