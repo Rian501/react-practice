de9104a083a6cf92d59702705d1cfa10bc9e46d0
Emily Lemmon <eclaire.lemmon@gmail.com>
2018-07-05 13:19:34 -0500

    employees on state

--- kennel/src/Kennel.js
+++ kennel/src/Kennel.js
@@ -4,12 +4,27 @@ import LocationList from './LocationList';
 
 
 export default class Kennel extends Component {
+
+
+  state = {
+    employees: [
+      { name: "Jessica Younker" },
+      { name: "Jordan Nelson" },
+      { name: "Zoe LeBlanc" },
+      { name: "Blaise Roberts" }
+    ],
+    locations: [
+      { name: "Nashville North" },
+      { name: "Nashville South" }
+    ]
+  }
+
   render() {
     return (
-      <div>
-        <LocationList />
-        <EmployeeList />
-      </div>
+      <React.Fragment>
+        <LocationList locations={this.state.locations} />
+        <EmployeeList employees={this.state.employees} />
+      </React.Fragment>
     );
   }
 }
